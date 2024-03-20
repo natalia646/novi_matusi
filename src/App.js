6add9afa180560842129bc3ae05363b61dafdf33
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-03-20 12:06:06 +0200

    add top part to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import TopPart from "./components/TopPart/TopPart";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <TopPart/>
+      </main>
     </>
   );
 }
